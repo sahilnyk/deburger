7b81c3194c16482456bb615131db3d70190706df
sahilnyk <contactsahilpnayak@gmail.com>
2026-05-15 22:13:12 +0530

    add fix data model

--- src/deburger/models/__init__.py
+++ src/deburger/models/__init__.py
@@ -2,5 +2,13 @@
 
 from deburger.models.error import ErrorInfo, ErrorClassification, TracebackFrame
 from deburger.models.test_result import TestResult
+from deburger.models.fix import Fix, CodeChange
 
-__all__ = ["ErrorInfo", "ErrorClassification", "TracebackFrame", "TestResult"]
+__all__ = [
+    "ErrorInfo",
+    "ErrorClassification",
+    "TracebackFrame",
+    "TestResult",
+    "Fix",
+    "CodeChange",
+]
