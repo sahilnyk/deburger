3841b29924b4c62968fc5bbea314ff686fbf2e3f
sahilnyk <contactsahilpnayak@gmail.com>
2026-05-15 21:48:53 +0530

    add test result model

--- src/deburger/models/__init__.py
+++ src/deburger/models/__init__.py
@@ -1,5 +1,6 @@
 """Data models for deburger."""
 
 from deburger.models.error import ErrorInfo, ErrorClassification, TracebackFrame
+from deburger.models.test_result import TestResult
 
-__all__ = ["ErrorInfo", "ErrorClassification", "TracebackFrame"]
+__all__ = ["ErrorInfo", "ErrorClassification", "TracebackFrame", "TestResult"]
