f1f9d52e15ef7151552e6b295fbc81d6b6fda963
sahilnyk <contactsahilpnayak@gmail.com>
2026-05-25 22:38:54 +0530

    fix: add _version.py, fix entry point

--- src/deburger/__init__.py
+++ src/deburger/__init__.py
@@ -3,9 +3,9 @@
 from importlib.metadata import PackageNotFoundError, version
 
 try:
-	__version__ = version("deburger")
+    __version__ = version("deburger")
 except PackageNotFoundError:
-	__version__ = "0.0.0"
+    from deburger._version import __version__
 
 __author__ = "Sahil Nayak"
 __email__ = "sahilnayak2056@gmail.com"
